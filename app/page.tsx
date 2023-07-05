98f230bccea728dc2c9746b038e3b8d423413a3f
Vadimcha <BelovBadim2014@yandex.ru>
2023-07-05 22:35:08 +0300

    “Made_the_main_page_a_bit”

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+import './main.css';
+
 export default function Home() {
-	return <h1>Welcome to IT_BELL</h1>;
+	const someNews = [{
+		id: 0,
+		title: 'Новость 1',
+		date: '01.01.23',
+		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
+		img: 'Image Link'
+	},
+	{
+		id: 1,
+		title: 'Новость 2',
+		date: '02.01.23',
+		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
+		img: 'Image Link'
+	},]
+
+	return <div className="content">
+		<section>
+			<h2 className='section-title'>Последние новости</h2>
+			{ someNews.map((news) => {	return <div className="last-news" key={ news.id }>
+					<h3 className="last-news__title">{ news.title }</h3>
+					<p className="last-news__dscr">{ news.dscr }</p>
+				</div> 
+			})}
+		</section>
+		<section>
+			<h2 className='section-title'>Может быть интересно</h2>
+			{ someNews.map((news) => { return <div className="rec-news" key={ news.id }>
+					<div className="text">
+						<h3 className="rec-news__title">{ news.title }</h3>
+						<p className="rec-news__date">{ news.date }</p>
+						<p className="rec-news_dscr">{ news.dscr }</p>
+					</div>
+					<p>{ news.img }</p>
+				</div>
+			})}
+		</section>
+	</div>;
 }
