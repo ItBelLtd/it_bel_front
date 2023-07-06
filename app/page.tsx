7611b948bb682c3d192b92922b919dbaca9f7713
Vadimcha <BelovBadim2014@yandex.ru>
2023-07-06 22:29:17 +0300

    Remade_main_page_using_Antd

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,43 @@
-import './main.css';
+'use client'
+import { Typography } from 'antd/es';
+import MyCarousel from './../components/MyCarousel';
+import { News } from '@/models/News';
+
+const { Title, Text } = Typography;
 
 export default function Home() {
-	const someNews = [{
+	const someNews : News[] = [{
 		id: 0,
 		title: 'Новость 1',
 		date: '01.01.23',
 		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
-		img: 'Image Link'
+		img: 'Image 1'
 	},
 	{
 		id: 1,
 		title: 'Новость 2',
 		date: '02.01.23',
 		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
-		img: 'Image Link'
+		img: 'Image 2'
 	},]
 
-	return <div className="content">
-		<section>
-			<h2 className='section-title'>Последние новости</h2>
-			{ someNews.map((news) => {	return <div className="last-news" key={ news.id }>
-					<h3 className="last-news__title">{ news.title }</h3>
-					<p className="last-news__dscr">{ news.dscr }</p>
-				</div> 
-			})}
-		</section>
-		<section>
-			<h2 className='section-title'>Может быть интересно</h2>
-			{ someNews.map((news) => { return <div className="rec-news" key={ news.id }>
-					<div className="text">
-						<h3 className="rec-news__title">{ news.title }</h3>
-						<p className="rec-news__date">{ news.date }</p>
-						<p className="rec-news_dscr">{ news.dscr }</p>
+	return <div className="content" style={{ padding: '35px 165px 50px 165px' }}>
+		<div className="last-news" style={{ marginBottom: '80px' }}>
+			<Title level={3} style={{ color: 'white', marginBottom: '20px' }}>Последние новости</Title>
+			<MyCarousel news={someNews} />
+		</div>
+		<div className="rec-news">
+			<Title level={3} style={{ color: 'white', marginBottom: '20px' }}>Может быть интересно</Title>
+			{ someNews.map(article => (
+				<div className="rec-news__item" key={ article.id } style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+					<div className="text" style={{ display: 'flex', flexDirection: 'column' }}>
+						<Title level={4} style={{ color: 'white' }}>{ article.title }</Title>
+						<Text type='secondary' style={{ color: '#676767' }}>{ article.date }</Text>
+						<Text style={{ color: 'white' }}>{ article.dscr }</Text>
 					</div>
-					<p>{ news.img }</p>
+					<Text mark strong>{ article.img }</Text>
 				</div>
-			})}
-		</section>
+			))}
+		</div>
 	</div>;
 }
---
+++ models/News.ts
@@ -0,0 +1,7 @@
+export interface News {
+  id: number,
+  title: string,
+  date: string,
+	dscr: string,
+	img: string,
+}
