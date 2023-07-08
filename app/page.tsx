affa640ac0e15788f9111ff5a7ac74854aa2ee20
Vadimcha <BelovBadim2014@yandex.ru>
2023-07-08 20:28:49 +0300

    Подправил косяки

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 import { Typography } from 'antd/es';
 import MyCarousel from '@/components/MyCarousel';
 import { News } from '@/models/News';
-import './main.scss'
+import './main.scss';
+import { Image } from 'antd/es';
 
 const { Title, Text } = Typography;
 
@@ -12,31 +13,37 @@ export default function Home() {
 		title: 'Новость 1',
 		date: '01.01.23',
 		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
-		img: 'Image 1'
+		img: 'https://farm2.staticflickr.com/1949/45717354341_a8dc471d63_b.jpg'
 	},
 	{
 		id: 1,
 		title: 'Новость 2',
 		date: '02.01.23',
 		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
-		img: 'Image 2'
+		img: 'https://st.depositphotos.com/1011976/2987/i/450/depositphotos_29872557-stock-photo-old-bridge-over-the-river.jpg'
 	},]
 
 	return <div className="content">
 		<div className="section">
-			<Title level={3} className='section__title' style={{ color: 'white' }}>Последние новости</Title>
+			<Title level={3} className='section__title'>Последние новости</Title>
 			<MyCarousel news={someNews} />
 		</div>
+		
 		<div className="section">
-			<Title level={3} className='section__title' style={{ color: 'white' }}>Может быть интересно</Title>
+			<Title level={3} className='section__title'>Может быть интересно</Title>
 			{ someNews.map(article => (
 				<div className="rec-news" key={ article.id }>
 					<div className="rec-news__text">
-						<Title level={4} style={{ color: 'white' }}>{ article.title }</Title>
-						<Text type='secondary' style={{ color: '#676767' }}>{ article.date }</Text>
-						<Text style={{ color: 'white' }}>{ article.dscr }</Text>
+						<Title level={4} className="rec-news__title">{ article.title }</Title>
+						<Text type='secondary' className="rec-news__date">{ article.date }</Text>
+						<Text className="rec-news__content">{ article.dscr }</Text>
 					</div>
-					<Text mark strong className='rec-news__image'>{ article.img }</Text>
+					<Image 
+						src={article.img}
+						width={350}
+						height={240}
+            preview={false}
+					/>
 				</div>
 			))}
 		</div>
--- components/MyCarousel.tsx
+++ components/MyCarousel.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react';
 import { Carousel } from 'antd/es';
 import { News } from '@/models/News';
