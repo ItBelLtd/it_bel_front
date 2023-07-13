34b7d02e0233a26a88253d3e3b30a5859ec2757c
Vadimcha <BelovBadim2014@yandex.ru>
2023-07-13 11:27:33 +0300

    Сделал карусель

--- components/MainPageComponents/Carousel/Carousel.tsx
+++ components/MainPageComponents/Carousel/Carousel.tsx
@@ -1,45 +1,125 @@
 'use client'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Pagination, Navigation, Autoplay } from 'swiper/modules';
-
-import React, { useRef, useState } from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import styles from './Carousel.module.css'
-import {News} from "@/models/News";
 import someNews from "@/data/News";
+import Image from 'next/image';
+import Link from "next/link";
 
-const Carousel = () => {
+const css = `
+    .react-multi-carousel-dot-list {
+        align-items: center;
+        gap: 15px;
+        justify-content: flex-start !important;
+        left: 115px !important;
+        bottom: 40px !important;
+    }
+`
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 1
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
 
+const CustomRightArrow = ({ onClick, ...rest } : any) => {
+    const {
+        onMove,
+        carouselState: { currentSlide, deviceType }
+    } = rest;
+    return <button onClick={() => onClick()} className={styles.right_arrow}>
+        <Image
+            width={12}
+            height={18}
+            src={'/RightArrow.svg'}
+            alt={'Next'}
+        />
+    </button>;
+};
+const CustomLeftArrow = ({ onClick, ...rest } : any) => {
+    const {
+        onMove,
+        carouselState: { currentSlide, deviceType }
+    } = rest;
+    return <button onClick={() => onClick()} className={styles.left_arrow}>
+        <Image
+            width={12}
+            height={18}
+            src={'/LeftArrow.svg'}
+            alt={'Prev'}
+        />
+    </button>;
+};
+const CustomDot = ({ onClick, ...rest } : any) => {
+    const {
+        onMove,
+        index,
+        active,
+        carouselState: { currentSlide, deviceType }
+    } = rest;
     return (
-        <>
-            <Swiper
-                pagination={{
-                    clickable: true
-                }}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
-                loop={true}
-                modules={[Pagination, Navigation, Autoplay]}
-                className={ styles.Swiper }
-            >
-                { someNews.map((news: News) => {
-                    return (
-                        <SwiperSlide className={ styles.news } key={ news.id }>
-                            <img src={news.img} alt="" className={styles.img}/>
-                            <div className={styles.text}>
-                                <h3 className={styles.title}>{ news.title }</h3>
-                                <p className={styles.dscr}>{ news.dscr }</p>
-                            </div>
-                        </SwiperSlide>
-                    )
-                })};
-            </Swiper>
-        </>
+        <div
+            className={`${active ? styles.active_dot : ''} ${styles.dot}`}
+            onClick={() => onClick()}
+        />
     );
 };
 
-export default Carousel;
+const MyCarousel = () => {
+    return (
+        <>
+            <Carousel
+                swipeable={false}
+                draggable={false}
+                responsive={responsive}
+                infinite={true}
+                customRightArrow={<CustomRightArrow />}
+                customLeftArrow={<CustomLeftArrow />}
+                showDots
+                customDot={<CustomDot />}
+                className={styles.carousel}
+                autoPlay={ true }
+                autoPlaySpeed={3000}
+            >
+                {someNews.map(news => {
+                    return (
+                        <Link href={`news/${String(news.id)}`} key={news.id} className={styles.news}>
+                            <div className={styles.left_part}>
+                                <div className={styles.top_part}>
+                                    <h4 className={styles.author_name}>{ news.authorName }</h4>
+                                    <h4 className={styles.date}>{ news.date }</h4>
+                                </div>
+                                <h3 className={styles.title}>{ news.title }</h3>
+                                <div className="controllers" />
+                            </div>
+                            <div className={styles.right_part}>
+                                <Image
+                                    width={445}
+                                    height={270}
+                                    src={news.img}
+                                    alt={'image'}
+                                />
+                            </div>
+                        </Link>
+                    )
+                })}
+            </Carousel>
+            <style>{css}</style>
+        </>
+    )
+};
+
+export default MyCarousel;
--- data/News.tsx
+++ data/News.tsx
@@ -1,12 +1,13 @@
-import { News } from '@/models/News';
+import {News} from "@/models/News";
 
 const someNews: News[] = [
 	{
 		id: 0,
-		title: 'Новость 1',
+		title: 'Новость 1 sdafffffffasdf  asdfasdfasdf  asdf',
 		date: '01.01.23',
 		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
 		img: 'https://farm2.staticflickr.com/1949/45717354341_a8dc471d63_b.jpg',
+		authorName: 'Lucas Clifford'
 	},
 	{
 		id: 1,
@@ -14,6 +15,7 @@ const someNews: News[] = [
 		date: '02.01.23',
 		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
 		img: 'https://st.depositphotos.com/1011976/2987/i/450/depositphotos_29872557-stock-photo-old-bridge-over-the-river.jpg',
+		authorName: 'Margaret Fisher'
 	},
 	{
 		id: 2,
@@ -21,6 +23,7 @@ const someNews: News[] = [
 		date: '03.01.23',
 		dscr: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil odit, vero voluptates beatae dignissimos sed harum praesentium molestias aspernatur quo.',
 		img: 'https://bipbap.ru/wp-content/uploads/2017/04/0_19347_bcb12d46_XL.jpg',
+		authorName: 'Dominique Collins'
 	},
 ];
 
