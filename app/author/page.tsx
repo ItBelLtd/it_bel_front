6820ec443c99a2bf8cb9df2864d36666cf35654b
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-07-23 22:44:40 +0600

    Add a tab switching implementation for the Author's page

--- app/author/page.tsx
+++ app/author/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
 import AuthorCard from '@/components/AuthorCard';
-import TabPanels from '@/components/TabPanels';
+import Tabs from '@/components/Tabs';
 import avatar from '@/public/author-avatar-slug.png';
-import styles from './author.module.css'
+import styles from './author.module.css';
 
 export const metadata: Metadata = {
   title: 'IT_BEL | Author',
@@ -11,8 +11,13 @@ export const metadata: Metadata = {
 const Author = () => {
   return (
     <div className={styles.container}>
-      <AuthorCard avatarURL={avatar.src} name={'Иван Иванов'} contact={'@ivanI'} authorID={1} />
-      <TabPanels />
+      <AuthorCard
+        avatarURL={avatar.src}
+        name={'Иван Иванов'}
+        contact={'@ivanI'}
+        authorID={1}
+      />
+      <Tabs />
     </div>
   );
 };
