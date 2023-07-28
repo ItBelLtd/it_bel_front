efb25eca9a86126c2bacd87c8ca344f3b032ce65
Vadimcha <BelovBadim2014@yandex.ru>
2023-07-28 14:45:10 +0300

    Подправил формат кода, попроавил отображение слайдера

--- components/Comment/index.tsx
+++ components/Comment/index.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import styles from './comment.module.css'
-import {Comment} from "@/models/Comment";
-import Image from "next/image";
+import styles from './comment.module.css';
+import {Comment} from '@/models/Comment';
+import Image from 'next/image';
 
 interface CommentProps {
     comment: Comment
 }
 
 const Comment = ({ comment } : CommentProps) => {
-    return (
-        <div className={styles.comment} >
-            <Image
-                src={'/userAvatar.jpg'}
-                width={50}
-                height={50}
-                alt={'Commentator avatar'}
-                className={styles.commentImg}
-            />
-            <div className={styles.commentContent}>
-                <div className={styles.commentTopPart}>
-                    <p className={styles.commentAuthor}>{comment.author}</p>
-                    <p className={styles.commentDate}>{comment.added}</p>
-                </div>
-                <p className={styles.commentText}>{comment.text}</p>
-            </div>
+  return (
+    <div className={styles.comment} >
+      <Image
+        src={'/userAvatar.jpg'}
+        width={50}
+        height={50}
+        alt={'Commentator avatar'}
+        className={styles.commentImg}
+      />
+      <div className={styles.commentContent}>
+        <div className={styles.commentTopPart}>
+          <p className={styles.commentAuthor}>{comment.author}</p>
+          <p className={styles.commentDate}>{comment.added}</p>
         </div>
-    );
+        <p className={styles.commentText}>{comment.text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Comment;
