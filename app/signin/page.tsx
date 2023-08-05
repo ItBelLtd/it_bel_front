b86ba634140e4abc4fe660e4d9e69869d3e469f4
Egor Dovgvillo <filimonchik1998@mail.ru>
2023-08-05 20:38:42 +0300

    Работа с ендпоинтами входа,регистрации, типизация хука запроса

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -7,69 +7,72 @@ import Link from 'next/link';
 import styles from './signin.module.css';
 import { SigninValues } from '@/models/Form';
 import { useState } from 'react';
+import { useStore } from '../store';
 
 export const metadata: Metadata = {
   title: 'IT_BEL | Sign in',
 };
 
 const Signin = () => {
+  const { signin, email, password, nonField } = useStore((state) => ({
+    signin: state.signin,
+    email: state.errors.email,
+    password: state.errors.password,
+    nonField: state.errors.non_field_errors,
+  }));
   const initialValues: SigninValues = {
     email: '',
     password: '',
   };
-  const [allError, setAllError] = useState({
-    email: '',
-    password: '',
-    non_field_errors: '',
-  });
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={async (values) => {
-        const response = await fetch('http://127.0.0.1/api/auth/token/login/', {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(values),
-        });
-        try {
-          if (!response.ok) {
-            const errors = await response.json();
-            if (errors) {
-              setAllError(errors);
-              console.log(allError);
-            }
-          }
-          const data = await response.json();
-          console.log(data.auth_token);
-          return data;
-        } catch (e) {
-          console.log('Произошла ошибка');
-        }
+      onSubmit={(values, e) => {
+        signin(values);
       }}
+      // const response = await fetch('http://127.0.0.1/api/auth/token/login/', {
+      //   method: 'POST',
+      //   mode: 'cors',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(values),
+      // });
+      // try {
+      //   if (!response.ok) {
+      //     const errors = await response.json();
+      //     if (errors) {
+      //       setAllError(errors);
+      //       console.log(allError);
+      //     }
+      //   }
+      //   const data = await response.json();
+      //   console.log(data.auth_token);
+      //   return data;
+      // } catch (e) {
+      //   console.log('Произошла ошибка');
+      // }
     >
       {({ isSubmitting }) => (
         <Form className={`${styles.form} + ${merriweather_sans.className}`}>
           <h3 className={`${styles.title} + ${roboto_mono.className}`}>
             С возвращением!
           </h3>
-          <div>{allError.non_field_errors}</div>
+          <div>{nonField}</div>
           <Field
             type='email'
             name='email'
             placeholder='Email'
             className={styles.input}
           />
-          <div>{allError.email}</div>
+          <div>{email}</div>
           <Field
             type='password'
             name='password'
             placeholder='Пароль'
             className={styles.input}
           />
-          <div>{allError.password}</div>
+          <div>{password}</div>
           <Link href='' className={styles.link}>
             Забыли пароль?
           </Link>
--- app/store.ts
+++ app/store.ts
@@ -1,13 +1,41 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import ItBelServices from '@/services/services';
 
-// type Store = {
-//   count: number;
-//   isActive: boolean;
-//   inc: () => void;
-// };
+type Store = {
+  errors: {
+    email: string;
+    password: string;
+    non_field_errors: string;
+  };
+  signin: any;
+  signup: any;
+};
 
-// export const useStore = create<Store>()((set) => ({
-//   count: 1,
-//   isActive: true,
-//   inc: () => set((state) => ({ count: state.count + 1 })),
-// }));
+export const useStore = create<Store>()(
+  devtools((set) => ({
+    errors: {
+      email: '',
+      password: '',
+      non_field_errors: '',
+    },
+    signin: async (values: object) => {
+      const { signin } = ItBelServices();
+      try {
+        const res = signin(values);
+        res.then((data) => set({ errors: data }));
+      } catch (error) {
+        /*какие-то действия */
+      }
+    },
+    signup: async (values: object) => {
+      const { signup } = ItBelServices();
+      try {
+        const res = signup(values);
+        res.then((data) => set({ errors: data }));
+      } catch {
+        /*какие-то действия */
+      }
+    },
+  })),
+);
