b5a3229f755a97679131c6691622fe921a7e0eaf
Vadimcha <BelovBadim2014@yandex.ru>
2023-08-08 22:39:29 +0300

    Corrected Authors, made the 'make_publish' page and moved everything from 'text-editors' there.

--- app/author/page.tsx
+++ app/author/page.tsx
@@ -1,52 +1,18 @@
 'use client';
 import { Metadata, NextPage } from 'next';
-// import AuthorCard from '@/components/AuthorCard';
-// import Tabs from '@/components/Tabs';
-// import TextEditor1 from '@/components/TextEditor1';
-// import TextEditor2 from '@/components/TextEditor2';
-// import avatar from '@/public/author-avatar-slug.png';
 import styles from './author.module.css';
-import { useAuthors } from '../stores/store';
+import {useAuthors} from '@/app/stores/authorsStore';
 import { useEffect } from 'react';
-import Link from 'next/link';
-import { Author } from '@/models/Authors';
-// import TextEditor from '@/components/TextEditor2';
-
-export const metadata: Metadata = {
-  title: 'IT_BEL | Author',
-};
+import { Author, Authors } from '@/models/Authors';
 
 const Author: NextPage = () => {
-  const { authors, getAllAuthors } = useAuthors((state) => ({
-    getAllAuthors: state.getAllAuthors,
-    authors: state.authors,
-  }));
+  const { getAllAuthors, authors } = useAuthors(state => state);
   useEffect(() => {
-    getAllAuthors('authors/');
+    getAllAuthors();
   }, []);
-  function renderAuthors(data: Author[]) {
-    const authors = data.map((item) => {
-      return (
-        <Link key={item.id} href={item.link}>
-          {item.name}
-        </Link>
-      );
-    });
-    return <div className=''>{authors}</div>;
-  }
-  const authorsList = renderAuthors(authors);
   return (
     <div className={styles.container}>
-      {authorsList}
-      {/* <TextEditor1 /> */}
-      {/* <TextEditor /> */}
-      {/* <AuthorCard
-        avatarURL={avatar.src}
-        name={'Иван Иванов'}
-        contact={'@ivanI'}
-        authorID={1}
-      />
-      <Tabs /> */}
+      { authors.map((author : Author) => <p key={author.name}>{author.surname}</p>) }
     </div>
   );
 };
