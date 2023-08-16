b1b2f18d90e04f9c087a6113414394af026b0fc0
Egor Dovgvillo <filimonchik1998@mail.ru>
2023-08-16 19:37:24 +0300

    Correction of registration, creation of a request (logout)

--- components/TextEditor2/index.tsx
+++ components/TextEditor2/index.tsx
@@ -5,20 +5,40 @@ import dynamic from 'next/dynamic';
 
 const TextEditorJodit = () => {
   const JoditEditor = dynamic(() => import('jodit-react'), { ssr: false });
-  const editor = useRef(null);
+  // const editor = useRef(null);
   const [content, setContent] = useState('');
   console.log(content);
+  // const config = {
+  //   language: 'ru',
+  //   tabIndex: 1,
+
+  // };
   const config = {
-    language: 'ru',
-    tabIndex: 1,
+    uploader: {
+      insertImageAsBase64URI: true, // Вставлять изображения в виде base64
+      insertImageAsBase64Mime: 'image/png', // MIME тип изображений
+      filesVariableName: 'file', // Имя переменной для загрузки файла
+      language: 'ru',
+      events: {
+        files: {
+          beforeUpload: (files: any) => {
+            // Обработчик загрузки файлов
+            console.log('Files', files);
+          },
+        },
+      },
+    },
+  };
+  const handleChange = () => {
+    setContent(content);
   };
   return (
     <JoditEditor
-      ref={editor}
+      // ref={editor}
       value={content}
       config={config} // tabIndex of textarea
       // onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-      onChange={(content) => console.log(content)}
+      onChange={handleChange}
     />
   );
 };
