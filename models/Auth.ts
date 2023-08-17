ffb66537ec21337e4a3365450d3df9ff1e880078
Egor Dovgvillo <filimonchik1998@mail.ru>
2023-08-17 12:30:10 +0300

    fix authStore, rename author registration page

--- models/Auth.ts
+++ models/Auth.ts
@@ -7,6 +7,7 @@ interface PromiseError {
   errors: Errors;
 }
 export interface Auth {
+  userId: number ;
   token: string;
   errors: Errors | null;
   signin: (url: string, values?: object) => void;
